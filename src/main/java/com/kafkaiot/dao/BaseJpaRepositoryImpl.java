345b88e1f6e0abde12dfa39b043d0afd30c7d0ba
Алексеев Илья <ibalekseev@diasoft.ru>
2018-02-27 14:47:45 +0500

    querydsl

---
+++ src/main/java/com/kafkaiot/dao/BaseJpaRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.kafkaiot.dao;
+
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.QuerydslJpaRepository;
+import org.springframework.data.querydsl.EntityPathResolver;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+
+@NoRepositoryBean
+public class BaseJpaRepositoryImpl<T, ID extends Serializable> extends QuerydslJpaRepository<T, ID> implements BaseJpaRepository<T, ID> {
+
+    private final EntityManager em;
+
+    public BaseJpaRepositoryImpl(JpaEntityInformation<T, ID> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.em = entityManager;
+    }
+
+    public BaseJpaRepositoryImpl(JpaEntityInformation<T, ID> entityInformation, EntityManager entityManager, EntityPathResolver resolver) {
+        super(entityInformation, entityManager, resolver);
+        this.em = entityManager;
+    }
+
+    @Override
+    public void detach(T entity) {
+        em.detach(entity);
+    }
+
+    @Override
+    public void detach(Iterable<T> entities) {
+        entities.forEach(this::detach);
+    }
+}
