d76f9b5b941a772e91c66c8e7dfbe0e9f5d9177a
Алексеев Илья <ibalekseev@diasoft.ru>
2018-03-12 09:40:23 +0500

    парсер теплодатчика

--- src/main/java/com/kafkaiot/service/JsonEventParser.java
+++ src/main/java/com/kafkaiot/service/JsonEventParser.java
@@ -6,6 +6,9 @@ import com.kafkaiot.model.SenlabTEntity;
 import com.kafkaiot.pojo.DevEUI;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JsonEventParser {
 /*
 {
@@ -116,6 +119,98 @@ public class JsonEventParser {
         int startPos = hexData.length() - 8;
         int endPos = hexData.length();
         return Integer.parseUnsignedInt(hexData.substring(startPos, endPos), 16);
+    }
+
+    private int getPacketNumber(String hexData) {
+        return Integer.parseUnsignedInt(hexData.substring(0, 2), 16);
+    }
+
+    private float parseFloat(String hexData, int startPos, int endPos) {
+        return Float.intBitsToFloat(Integer.parseInt(hexData.substring(startPos, endPos), 16));
+    }
+
+    public Map<String, Object> getPacketData(String hexData) {
+        Map<String, Object> result = new HashMap<>();
+
+//Лог. адрес Физ. адрес Название параметра Тип Номер пакета Позиция Размер
+        switch (getPacketNumber(hexData)) {
+            case 1:
+//uint 1 0 1
+//300001 0x0000 Текущее время в сек ulong 1 1 4
+                result.put("time", Long.parseUnsignedLong(hexData.substring(2, 10), 16));
+//300003 0x0002 Значение канала Qt1 float 1 5 4
+                result.put("Qt1", parseFloat(hexData, 10, 18));
+//300005 0x0004 Значение канала Qt2 float 1 9 4
+                result.put("Qt2", parseFloat(hexData, 18, 26));
+//300007 0x0006 Значение канала Qt3 float 1 13 4
+                result.put("Qt3", parseFloat(hexData, 26, 34));
+//300009 0x0008 Значение канала Qt4 float 1 17 4
+                result.put("Qt4", parseFloat(hexData, 34, 42));
+//300011 0x000A Значение канала dQt1 float 1 21 4
+                result.put("dQt1", parseFloat(hexData, 42, 50));
+//300013 0x000C Значение канала dQt3 float 1 25 4
+                result.put("dQt3", parseFloat(hexData, 50, 58));
+//300015 0x000E Значение канала Wt1 float 1 29 4
+                result.put("Wt1", parseFloat(hexData, 58, 66));
+//300017 0x0010 Значение канала Wt2 float 1 33 4
+                result.put("Wt2", parseFloat(hexData, 66, 74));
+//300019 0x0012 Значение канала Wt3 float 1 37 4
+                result.put("Wt3", parseFloat(hexData, 74, 82));
+//300021 0x0014 Значение канала Wt4 float 1 41 2
+                result.put("Wt4", parseFloat(hexData, 82, 86));
+                break;
+            case 2:
+//uint 2 0 1
+//300021 0x0014 Значение канала Wt4 float 2 1 2
+                result.put("Wt4", parseFloat(hexData, 2, 6));
+//300023 0x0016 Значение канала T1 float 2 3 4
+                result.put("T1", parseFloat(hexData, 6, 14));
+//300025 0x0018 Значение канала T2 float 2 7 4
+                result.put("T2", parseFloat(hexData, 14, 22));
+//300027 0x001A Значение канала T3 float 2 11 4
+                result.put("T3", parseFloat(hexData, 22, 30));
+//300029 0x001C Значение канала T4 float 2 15 4
+                result.put("T4", parseFloat(hexData, 30, 38));
+//300031 0x001E Значение канала Qo1 float 2 19 4
+                result.put("Qo1", parseFloat(hexData, 38, 46));
+//300033 0x0020 Значение канала Qo2 float 2 23 4
+                result.put("Qo2", parseFloat(hexData, 46, 54));
+//300035 0x0022 Значение канала Qo3 float 2 27 4
+                result.put("Qo3", parseFloat(hexData, 54, 62));
+//300037 0x0024 Значение канала Qo4 float 2 31 4
+                result.put("Qo4", parseFloat(hexData, 62, 70));
+//300039 0x0026 Значение канала Qm1 float 2 35 4
+                result.put("Qm1", parseFloat(hexData, 70, 78));
+//300041 0x0028 Значение канала Qm2 float 2 39 4
+                result.put("Qm2", parseFloat(hexData, 78, 86));
+                break;
+            case 3:
+//uint 3 0 1
+//300043 0x002A Значение канала Qm3 float 2 1 4
+                result.put("Qm3", parseFloat(hexData, 2, 10));
+//300045 0x002C Значение канала Qm4 float 2 5 4
+                result.put("Qm4", parseFloat(hexData, 10, 18));
+//300047 0x002E Значение канала Gm1 float 2 9 4
+                result.put("Gm1", parseFloat(hexData, 18, 26));
+//300049 0x0030 Значение канала Gm2 float 2 13 4
+                result.put("Gm2", parseFloat(hexData, 26, 34));
+//300051 0x0032 Значение канала Gm3 float 3 17 4
+                result.put("Gm3", parseFloat(hexData, 34, 42));
+//300053 0x0034 Значение канала Gm4 float 3 21 4
+                result.put("Gm4", parseFloat(hexData, 42, 50));
+//300055 0x0036 Значение канала dGm1 float 3 25 4
+                result.put("dGm1", parseFloat(hexData, 50, 58));
+//300057 0x0038 Значение канала dGm3 float 3 29 4
+                result.put("dGm3", parseFloat(hexData, 58, 66));
+//300059 0x003A Значение канала P1 float 3 33 4
+                result.put("P1", parseFloat(hexData, 66, 74));
+//300061 0x003C Значение канала P2 float 3 37 4
+                result.put("P2", parseFloat(hexData, 74, 82));
+//300063 0x003E Значение канала P3 float 3 41 4
+                result.put("P3", parseFloat(hexData, 82, 90));
+                break;
+        }
+        return result;
     }
 
     public String getHexData() {
